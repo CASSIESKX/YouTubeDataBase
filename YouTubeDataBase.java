5fbfb4e7f43bca23800cdf17fa3ec4bc911d82b8
Cassie Shu <32623423+CASSIESKX@users.noreply.github.com>
2019-06-14 14:28:00 -0700

    Update YouTubeDataBase.java
    File Processing & Menu Selection

--- YouTubeDataBase.java
+++ YouTubeDataBase.java
@@ -1,46 +1,84 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 
 public class YouTubeDataBase {
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		Scanner input = new Scanner(new File("data.txt"));
-		String fullURL, url ="", title="", publisher="";
-		int year, views;
-		while(input.hasNextLine()) {
-			fullURL = input.nextLine();
-			for(int i = 0; i < fullURL.length(); i++) {
-				if (fullURL.charAt(i)==('=')) {
-					url = fullURL.substring(i+1, fullURL.length() - 1);
-				}
-			}
-			//System.out.println("URL: " + url);
-			
-			title = input.nextLine();
-			//System.out.println("Title: " + title);
-			
-			publisher = input.nextLine();
-			//System.out.println("Publisher: " + publisher);
-			
-			year = input.nextInt();
-			//System.out.println("Year" + year);
-			
-			views = input.nextInt();
-			//System.out.println("Views: " + views);
-			
-			if(input.hasNextLine()) {
-				input.nextLine();
-			}
-			
-			// create a new object & add data
-			
-		}	
-		input.close();
-		
-		PrintWriter output = new PrintWriter(new File("output.txt"));
-		// write data to file
-		output.println();
-		output.close();
+public static void main (String [] args) throws IOException{
+	HashTable <Video> data = new HashTable <> (75);
+	System.out.println("Welcome to the YouTube DataBase! ");
+	System.out.println("We are now reading data from our database ... ");
+	String inputFile = "data.txt";
+	File file = new File (inputFile);
+	Scanner fileRead = new Scanner (file);
+	//File reading Error: 0
+	if(!file.exists()) {
+		System.out.println("Error reading the data. Exit code: 0");
+		System.exit(0);
 	}
+	
+	//Data Processing
+	String videoURL;
+	String videoName;
+	String publisher;
+	String views;
+	String year;
+	while(fileRead.hasNext()) {
+		videoURL = fileRead.nextLine();
+		videoName = fileRead.nextLine();
+		publisher = fileRead.nextLine();
+		year = fileRead.nextLine();
+		views = fileRead.nextLine();
+		
+		Video tempVideo = new Video(videoURL, videoName, publisher, year, views);
+		data.insert(tempVideo);
+	}
+	
+	//UI part
+	String userChoice = "";
+	while(!userChoice.equalsIgnoreCase("x")){
+		printSelection();
+		String tempurl, tempname, temppublisher, tempyear, tempviews;
+		Scanner userInput = new Scanner (System.in);
+		if(userChoice.equalsIgnoreCase("A")) {
+			System.out.println("Now adding a video");
+			System.out.println("Enter the URL of the video: ");
+			tempurl = userInput.nextLine();
+			System.out.println("Enter the name of the video: ");
+			tempname = userInput.nextLine();
+			System.out.println("Enter the publisher of the video: ");
+			temppublisher = userInput.nextLine();
+			System.out.println("Enter the year of the video: ");
+			tempyear = userInput.nextLine();
+			System.out.println("Enter the views of the video: ");
+			tempviews = userInput.nextLine();
+			Video tempVideo = new Video (tempurl, tempname, temppublisher, tempyear, tempviews);
+			data.insert(tempVideo);
+		}else if(userChoice.equalsIgnoreCase("D")) {
+			System.out.println("Now deleting a video ... ");
+			System.out.println("Enter the URL of the video: ");
+			tempurl = userInput.nextLine();
+			System.out.println("Enter the name of the video: ");
+			tempname = userInput.nextLine();
+			Video tempVideo = new Video (tempurl, tempname);
+			
+			
+		}else if(userChoice.equalsIgnoreCase("S")) {
+			
+		}else if(userChoice.equalsIgnoreCase("L")) {
+			
+		}else if(!userChoice.equalsIgnoreCase("X")) {
+			//Invalid input
+			System.out.println("Invalid Input!");
+		}
+	}
+	
+}
 
+public static void printSelection () {
+	System.out.println("Please select your choice: ");
+	System.out.println("A. Let me add a new video! ");
+	System.out.println("D. Ehhh, I want to delete a video.");
+	System.out.println("S. I wanna search something ...");
+	System.out.println("L. List everything for me!");
+}
 }
