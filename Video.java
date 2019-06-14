7fdcf126a431fbdef1965101081778ae412ce436
SuzieC <51734967+SuzieC@users.noreply.github.com>
2019-06-14 13:22:33 -0700

    Update Video.java

--- Video.java
+++ Video.java
@@ -1,15 +1,100 @@
-
-public class Video {
+public class Video  implements Comparable<Video>{
 	private String url;
 	private String name;
 	private String publisher;
 	private String year;
 	private String views;
-	public Video (String a, String b, String c, String d, String e) {
-		url = a;
-		name = b;
-		publisher =c;
-		year = d;
-		views = e;
+	
+	/*** Default constructor ***/
+	public Video() {
+		this("Unknown URL", "Unknown Title", "Unknown publisher", "Unknown Year", "Unknown Views");
 	}
+	
+	/*** 5 parameters constructor ***/
+	public Video (String url, String name, String publisher, String year, String views) {
+		this.url = url;
+		this.name = name;
+		this.publisher =publisher;
+		this.year = year;
+		this.views = views;
+	}
+	
+	/*** Setters ***/
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public void setViews(String views) {
+		this.views = views;
+	}
+	
+	/*** Getters ***/
+	public String getUrl() {
+		return url;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	public String getViews() {
+		return views;
+	}
+
+	
+	/*** Creates a String of YouTubeDataBase information ***/
+    @Override public String toString() {
+        return "URL: " + url + "\n" + 
+        		"Video Title: " + name + "\n" + 
+        		"Publisher: " + publisher + "\n" + 
+        		"Year: " + year + "\n" +
+        		"Views: " + views + "\n"; 
+    }
+
+    @Override public boolean equals(Object o) {
+    	if(this == o) {
+    		return true;
+    	} else if (!(o instanceof Video)) {
+    		return false;
+    	} else {
+    		Video v = (Video) o;
+    		return url.equals(v.url) && name.equals(v.name) && publisher.equals(publisher) && year.equals(year) && views.equals(views);
+    	}
+    }
+    
+	@Override
+	public int compareTo(Video o) {
+	   	if(this.equals(o)){
+    		return 0;
+    	} else if (!url.equals(o.url)) {
+    		return url.compareTo(o.url);
+    	} else if (!name.equals(o.name)) {
+    		return name.compareTo(o.name);
+    	} else if (!publisher.equals(o.publisher)) {
+    		return publisher.compareTo(o.publisher);
+    	} else if (!year.equals(o.year)) {
+    		return year.compareTo(o.year);
+    	} else {
+    		return views.compareTo(o.views);
+    	}
+    }
+	
 }
