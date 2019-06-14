35fcb5c44f0d0c30b6ae47744bb1380a0e2c68af
Cassie Shu <32623423+CASSIESKX@users.noreply.github.com>
2019-06-13 23:49:24 -0700

    Not finished

---
+++ Video.java
@@ -0,0 +1,15 @@
+
+public class Video {
+	private String url;
+	private String name;
+	private String publisher;
+	private String year;
+	private String views;
+	public Video (String a, String b, String c, String d, String e) {
+		url = a;
+		name = b;
+		publisher =c;
+		year = d;
+		views = e;
+	}
+}
