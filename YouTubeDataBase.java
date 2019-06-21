cb748d94c8bd2cad12427176f7742602163d2a0f
Yodragon <49503358+Yodragon@users.noreply.github.com>
2019-06-21 15:54:06 -0700

    Update YouTubeDataBase.java

--- YouTubeDataBase.java
+++ YouTubeDataBase.java
@@ -24,7 +24,7 @@ public class YouTubeDataBase {
 		BST bst2 = new BST(false);//BST by video name
 
 		System.out.println("Welcome to the YouTube DataBase! ");
-		System.out.println("We are now reading data from our database...\n");
+		System.out.println("\nWe are now reading in videos from 'data.txt'...\n");
 		String inputFile = "data.txt";
 
 		File file = new File(inputFile);
@@ -65,7 +65,7 @@ public class YouTubeDataBase {
 		while (!userChoice.equalsIgnoreCase("x")) {
 			
 			if (userChoice.equalsIgnoreCase("A")) {
-				System.out.println("\nNow adding a video");
+				System.out.println("\nNow adding a video...");
 				while (true) {
 					System.out.println("\nNote: The input URL doesn't have youtube host and path. They are added automatically.");
 					System.out.println("So only add the LAST part of the URL.\n");
@@ -73,7 +73,7 @@ public class YouTubeDataBase {
 					tempurl = userInput.nextLine();
 					if (!tempurl.startsWith("https://www.youtube.com/watch?v=")) {
 						tempurl = "https://www.youtube.com/watch?v=" + tempurl;
-						System.out.println("The url stored in database is: " + tempurl);
+						//System.out.println("The url stored in database is: " + tempurl);
 					}
 					System.out.print("Enter the name of the video: ");
 					tempname = userInput.nextLine();
@@ -82,14 +82,14 @@ public class YouTubeDataBase {
 					System.out.print("Enter the year of the video: ");
 					tempyear = userInput.nextLine();
 					if (!isInteger(tempyear)) {
-						System.out.println("\"Year\" needs to be a non-negative integer");
+						System.out.println("\n\"Year\" needs to be a non-negative integer");
 						System.out.println("Please provide info again:");
 						continue;
 					}
 					System.out.print("Enter the views of the video: ");
 					tempviews = userInput.nextLine();
 					if (!isInteger(tempviews)) {
-						System.out.println("\"Views\" needs to be a non-negative integer");
+						System.out.println("\n\"Views\" needs to be a non-negative integer.");
 						System.out.println("Please provide info again:");
 						continue;
 					} else {
@@ -101,69 +101,70 @@ public class YouTubeDataBase {
 				if (bst1.insert(tempVideo)) {
 					h.insert(tempVideo); // add to HashTable h1
 					bst2.insert(tempVideo); // add to BST1
-					System.out.println("Added successfully!");
+					System.out.println("\nAdded successfully!");
 				} else {
-					System.out.println("URL <" + tempurl + "> already exists. Added failed");
+					System.out.println("\nURL <" + tempurl + "> already exists. Could not add video.");
 				}
 				System.out.println();
 				
 			} else if (userChoice.equalsIgnoreCase("D")) {
-				System.out.println("\nNow deleting a video ... ");
+				System.out.println("\nNow deleting a video... ");
 				System.out.println("\nNote: The input URL doesn't have youtube host and path. They are added automatically.");
 				System.out.println("So only add the LAST part of the URL.\n");
 				System.out.print("Enter the URL of the video: ");
 				tempurl = userInput.nextLine();
 				if (!tempurl.startsWith("https://www.youtube.com/watch?v=")) {
 					tempurl = "https://www.youtube.com/watch?v=" + tempurl;
-					System.out.println("The url to remove in database is: " + tempurl);
+					System.out.println("\nThe url to remove in database is: " + tempurl);
 				}
 				Video tempVideo = new Video (tempurl, "unknown name");
 				//should call remove method to remove data
 				Video v = h.search(tempVideo);
 				if(v == null) {
-					System.out.println("The video you are looking for is not in the database!");
+					System.out.println("\nThe video you are looking for is not in the database!");
 				} else {
 					bst1.remove(v);
 					bst2.remove(v);
             		h.remove(v);
-            		System.out.println("The video: <" + v.getName() +"> has been removed!");
+            		System.out.println("\nThe video: <" + v.getName() +"> has been removed!");
 				}
 				System.out.println();
 			} else if (userChoice.equalsIgnoreCase("S")) {
 				//Sub menu here
-				System.out.println("Would you like to search using URL or video name?");
-				System.out.println("Enter U for URL, otherwise, the program will search by name! ");
-				System.out.print(">>>>");
+				System.out.println("\nNow searching the database...");
+				System.out.println("\nWould you like to search using URL or video name?");
+				System.out.println("Enter 'U' for URL, otherwise the program will search by name! ");
+				System.out.print("\nEnter choice here: ");
 				Video tempVideo = new Video();
 				String searchResponse = userInput.nextLine();
 				if (searchResponse.equalsIgnoreCase("u")) {
-					System.out.println("Now searching based on URL! ");
+					System.out.println("\nNow searching based on URL! ");
 					System.out.println("\nNote: The input URL doesn't have youtube host and path. They are added automatically.");
 					System.out.println("So only add the LAST part of the URL.\n");
 					System.out.print("Enter the URL please: ");
 					String urlFromUser = userInput.nextLine();
 					if (!urlFromUser.startsWith("https://www.youtube.com/watch?v=")) {
 						urlFromUser = "https://www.youtube.com/watch?v=" + urlFromUser;
-						System.out.println("The url to search in database is: " + urlFromUser);
+						System.out.println("\nThe url to search in database is: " + urlFromUser);
 					}
 					tempVideo.setUrl(urlFromUser);
 					Video v = h.search(tempVideo);
 					if (v == null) {
-						System.out.println("Cannot find video with url <" + urlFromUser +">");
+						System.out.println("\nCannot find video with url <" + urlFromUser +">");
 					} else {
-						System.out.println("Found video with url <" + urlFromUser + "> below:");
+						System.out.println("\nFound video with url <" + urlFromUser + "> below:\n");
 						System.out.println(v);
 					}
 				} else {
-					System.out.println("Now searching based on video names! ");
-					System.out.print("Enter the video name please :");
+					System.out.println("\nNow searching based on video names! ");
+					System.out.print("\nEnter the video name please: ");
 					String nameFromUser = userInput.nextLine();
 					tempVideo.setName(nameFromUser);
 					ArrayList<Video> al = bst2.searchByVideoName(tempVideo);
 					if (al.size() == 0) {
-						System.out.println("Cannot find video with name <" + nameFromUser + ">");
+						System.out.println("\nCannot find video with name <" + nameFromUser + ">");
 					} else {
-						System.out.println("Found video with name <" + nameFromUser + "> below:");
+						System.out.println("\nFound video with name <" + nameFromUser + "> below:\n");
 						for (int i = 0; i < al.size(); i++) {
 							System.out.println(al.get(i));
 						}
@@ -172,12 +173,13 @@ public class YouTubeDataBase {
 				System.out.println("");
 
 			} else if (userChoice.equalsIgnoreCase("L")) {
-				System.out.println("Now printing the database! ");
-				System.out.println("Enter 'U' to print unsorted data, ");
+				System.out.println("\nNow printing the database...");
+				System.out.println("\nEnter 'U' to print unsorted data, ");
 				System.out.println("'R' to print the data sorted by URL,");
-				System.out.println("otherwise, the program will print data sorted by video names.");
-				System.out.print(">>>>");
+				System.out.println("otherwise the program will print data sorted by video names.");
+				System.out.print("\nEnter your choice here: ");
 				String printResponse = userInput.nextLine();
+				System.out.println();
 				if(printResponse.equalsIgnoreCase("U")) {
 					h.printTable();
 				}else if(printResponse.equalsIgnoreCase("R")) {
@@ -187,7 +189,7 @@ public class YouTubeDataBase {
 				}
 				System.out.println();
 			} else if (userChoice.equalsIgnoreCase("W")){
-				System.out.println("Now writing data into output.text....");
+				System.out.println("\nNow writing data into 'output.txt'...");
 				PrintWriter output = new PrintWriter(new File("output.txt"));
 				//open a file for read
 				for(int i = 0; i < h.tableSize(); i++) {
@@ -201,7 +203,7 @@ public class YouTubeDataBase {
 					}
 				}
 				output.close();
-				System.out.println("Writing done!");
+				System.out.println("\nWriting done!");
 				System.out.println();
 			} else {
 				// Invalid input
@@ -214,7 +216,8 @@ public class YouTubeDataBase {
 			userInput.nextLine();
 
 		}
-		System.out.println("\nGood Bye!");  // user enter x to exit the program
+		System.out.println("\nNow writing into 'autosaved.txt'...");
+		System.out.println("\nGoodbye!");  // user enter x to exit the program
 				
 		PrintWriter output = new PrintWriter(new File("autosaved.txt"));
 		//open a file for read
